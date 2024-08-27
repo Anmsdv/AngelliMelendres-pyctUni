bc884560d7e5b100a5f19b70640f6cd709bb320c
Anmsdv <angellimsilvestre@gmail.com>
2024-08-27 14:10:53 -0500

    Primer avance del proyecto

---
+++ javaScript/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const sidebar = document.querySelector('.sidebar');
+    const content = document.querySelector('.content');
+    const header = document.querySelector('header');
+
+    menuToggle.addEventListener('click', () => {
+        sidebar.classList.toggle('collapsed');
+        content.classList.toggle('expanded');
+        header.classList.toggle('expanded');
+    });
+});
